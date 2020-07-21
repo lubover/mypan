f1b9579a32e92f40445f8e4a14ef47d946aaf9c9
lubover <57246011+lubover@users.noreply.github.com>
2020-07-20 20:20:21 -0500

    Add files via upload

---
+++ config/base.php
@@ -0,0 +1,118 @@
+<?php return array (
+  'qqlogin' => '',
+  'cssstyle' => '',
+  'guestpreload' => '1',
+  'adminpreload' => '1',
+  'bgimg' => 'https://cdn.jsdelivr.net/gh/742481030/cdnimg@master/E2JNM8.jpg',
+  'mobileimg' => 'https://cdn.jsdelivr.net/gh/SeireiA/Seirei@latest/usr/uploads/2020/07/1366416900.png',
+  'update' => 'on',
+  'guestupload' => '',
+  'site_name' => 'superone',
+  'title_name' => '',
+  'appbar' => '',
+  'password' => 'a787461878',
+  'drawer' => '',
+  'style' => 'nexmoe',
+  'onedrive_root' => '',
+  'cache_type' => 'filecache',
+  'cache_expire_time' => 3600,
+  'cache_refresh_time' => 600,
+  'page_item' => 50,
+  'root_path' => '',
+  'onedrive_hide' => 'uploads',
+  'show' => 
+  array (
+    'stream' => 
+    array (
+      0 => 'txt',
+      1 => 'html',
+    ),
+    'image' => 
+    array (
+      0 => 'bmp',
+      1 => 'jpg',
+      2 => 'jpeg',
+      3 => 'png',
+      4 => 'gif',
+      5 => 'webp',
+    ),
+    'video5' => 
+    array (
+    ),
+    'video' => 
+    array (
+      0 => 'mpg',
+      1 => 'mpeg',
+      2 => 'mov',
+      3 => 'flv',
+      4 => 'mp4',
+      5 => 'webm',
+      6 => 'mkv',
+      7 => 'm3u8',
+    ),
+    'video2' => 
+    array (
+      0 => 'avi',
+      1 => 'rm',
+      2 => 'rmvb',
+      3 => 'wmv',
+      4 => 'asf',
+      5 => 'ts',
+    ),
+    'audio' => 
+    array (
+      0 => 'ogg',
+      1 => 'mp3',
+      2 => 'wav',
+      3 => 'flac',
+      4 => 'aac',
+      5 => 'm4a',
+      6 => 'ape',
+    ),
+    'code' => 
+    array (
+      0 => 'php',
+      1 => 'css',
+      2 => 'go',
+      3 => 'java',
+      4 => 'js',
+      5 => 'json',
+      6 => 'txt',
+      7 => 'sh',
+      8 => 'md',
+    ),
+    'doc' => 
+    array (
+      0 => 'csv',
+      1 => 'doc',
+      2 => 'docx',
+      3 => 'odp',
+      4 => 'ods',
+      5 => 'odt',
+      6 => 'pot',
+      7 => 'potm',
+      8 => 'potx',
+      9 => 'pps',
+      10 => 'ppsx',
+      11 => 'ppsxm',
+      12 => 'ppt',
+      13 => 'pptm',
+      14 => 'pptx',
+      15 => 'rtf',
+      16 => 'xls',
+      17 => 'xlsx',
+    ),
+  ),
+  'images' => 
+  array (
+    'home' => false,
+    'public' => false,
+    'exts' => 
+    array (
+      0 => 'jpg',
+      1 => 'png',
+      2 => 'gif',
+      3 => 'bmp',
+    ),
+  ),
+);
